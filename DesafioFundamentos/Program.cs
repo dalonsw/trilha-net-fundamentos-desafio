383d79dfa111ba0a45a7a3f7fb1466be163153ed
Daniel <danielalonso.os@gmail.com>
2024-09-26 20:03:48 -0300

    Desafio Concluído

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,10 +1,13 @@
-﻿using DesafioFundamentos.Models;
+﻿using System;
+using DesafioFundamentos.Models;
 
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
+
+Console.Clear();
 
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
